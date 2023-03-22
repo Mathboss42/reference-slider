dbfef408282c626ce5763e9444d0da70e4cb2423
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-03-22 16:42:41 +0100

    Set up project boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# reference-slider
+
+Image slider with timer that displays a different image every n seconds to practice sketching / gesture drawing.
---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import router from '.././router';
+
+const time = ref(60);
+
+function startSession() {
+    console.log('start session');
+    router.push({ name: 'session', params: { time: time.value}});
+}
+
+</script>
+
+<template>
+  <main>
+    <button 
+        @click="startSession"
+        class="btn btn-primary"
+    >
+        Start
+    </button>
+  </main>
+</template>
+
+<style scoped>
+main {
+    padding-top: 48px;
+    display: flex;
+    justify-content: center;
+}
+</style>
