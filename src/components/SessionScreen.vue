eb93244028e0cb2af733640527790cfe5c2e3257
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-03-23 21:42:52 +0100

    Set up previous, next and play/pause functionalities

--- src/components/SessionScreen.vue
+++ src/components/SessionScreen.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineProps, onMounted, watch, ref, computed } from 'vue';
+import { defineProps, onMounted, ref, computed, onUnmounted } from 'vue';
 import images from '../assets/images';
 
 // eslint-disable-next-line no-unused-vars
@@ -24,7 +24,9 @@ const imgs = computed(() => {
 })
 
 let index = 0;
-let isActive = true;
+let isActive = ref(true);
+let isPaused = ref(false);
+let interval;
 
 function shuffle(array) {
   let currentIndex = array.length,  randomIndex;
@@ -40,38 +42,102 @@ function shuffle(array) {
   return array;
 }
 
-function displayNextImage() {
-	if (index + 1 > imgs.value.length - 1) {
-		isActive = false;
+function onClickPrevious() {
+	if (isActive.value) {
+		displayPreviousImage()
+		resetInterval();
+	} else {
+		isActive.value = true;
+		index = imgs.value.length;
+		displayPreviousImage();
+		resetInterval();
+	}
+}
+
+function onClickNext() {
+	displayNextImage();
+	resetInterval();
+}
+
+function displayPreviousImage() {
+	if (index < 1) {
+		alert('This is the first image.')
 		return;
 	}
+	totalTime.value = props.time;
+	index--;
+}
+
+function displayNextImage() {
+	if (index + 1 > imgs.value.length - 1) {
+		isActive.value = false;
+		return;
+	}
+	totalTime.value = props.time;
 	index++;
 }
 
-onMounted(() => {
-	setTimeout(() => {
-		totalTime.value--;
-	}, 1000);
-})
-
-watch(totalTime, () => {
-	if (isActive) {
-		setTimeout(() => {
-			totalTime.value--;
-			if (totalTime.value < 0) {
-				displayNextImage();
-				totalTime.value = props.time;
-			}
-		}, 1000);
+function resetInterval() {
+	clearInterval(interval);
+	if (isActive.value) {
+		addInterval();
 	} else {
 		totalTime.value = 0;
 	}
+}
+
+function playPause() {
+	if (isActive.value) {
+		if (!isPaused.value) {
+			isPaused.value = !isPaused.value;
+			clearInterval(interval);
+		} else {
+			isPaused.value = !isPaused.value;
+			resetInterval();
+		}
+	} else {
+		return;
+	}
+}
+
+function addInterval() {
+	interval = setInterval(() => {
+		totalTime.value--;
+		if (totalTime.value < 0) {
+			displayNextImage()
+		}
+	}, 1000);
+}
+
+onMounted(() => {
+	addInterval();
+});
+
+onUnmounted(() => {
+	clearInterval(interval);
 });
 </script>
 
 <template>
 	<div class="session container">
 		<div class="row justify-content-center align-items-center mb-3 pt-3">
+			<div class="col-auto">
+				<button
+					class="btn btn-primary fs-4"
+					@click="onClickPrevious"
+				>
+					&lt;
+				</button>
+			</div>
+			<div class="col-auto">
+				<button
+					class="btn btn-primary fs-4"
+					@click="playPause"
+				>
+					<i v-if="!isPaused" class="fa-solid fa-pause"></i>
+					<i v-else class="fa-solid fa-play"></i>
+				</button>
+			</div>
 			<div class="col-auto">
 				<h1 class="text-white">Session</h1>
 			</div>
@@ -83,6 +149,14 @@ watch(totalTime, () => {
 					Quit
 				</button>
 			</div>
+			<div class="col-auto">
+				<button
+					class="btn btn-primary fs-4"
+					@click="onClickNext"
+				>
+					>
+				</button>
+			</div>
 		</div>
 		<div class="row justify-content-center align-items-center mb-5">
 			<div class="col-auto text-white fs-1 border">
@@ -90,7 +164,17 @@ watch(totalTime, () => {
 			</div>
 		</div>
 		<div class="d-flex justify-content-center align-items-center">
-			<img v-if="isActive" :src="imgs[index]" alt="user img">
+			<img v-if="isActive && !isPaused" :src="imgs[index]" alt="user img">
+			<div v-else-if="isActive && isPaused" class="col-auto">
+				<div class="row">
+					<p class="text-white fs-1">Session Paused</p>
+				</div>
+				<div class="row">
+					<div class="d-flex justify-content-center">
+						<button class="btn btn-primary fs-4" @click="restart">Restart</button>
+					</div>
+				</div>
+			</div>
 			<div v-else class="col-auto">
 				<div class="row">
 					<p class="text-white fs-1">End of Session!</p>
